9ec597eb3b0e43faaab49b41f45602932056dd67
Bernard Baker <bernard.baker.me@gmail.com>
2019-05-17 17:42:07 +0100

    Updated GraphQL endpoint.

--- src/apollo/index.js
+++ src/apollo/index.js
@@ -1,7 +1,10 @@
 import ApolloClient, { gql } from "apollo-boost";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:4000/graphql"
+      : "https://spotahome-server.herokuapp.com/graphql"
 });
 
 export const GET_HOME_CARDS_QUERY = gql`
